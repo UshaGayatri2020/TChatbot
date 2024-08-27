65805c5aee574b32fbc9c3173dab44aa19e45668
UshaGayatri2020 <ushagayatri@gmail.com>
2024-08-27 15:45:44 -0500

    updated

--- app.py
+++ app.py
@@ -1,18 +1,19 @@
 from flask import Flask, render_template, jsonify, request
 from dotenv import load_dotenv
 import os
-#from tradingbot.retrieval_generation import generation
-#from tradingbot.data_ingestion import ingestdata
+from tradingbot.retrieval_generation import generation
+from tradingbot.data_ingestion import ingestdata
 
 app = Flask(__name__)
 
 load_dotenv()
 
-#vstore=ingestdata("done")
-#chain=generation(vstore)
+vstore=ingestdata("done")
+langachain=generation(vstore)
 
 @app.route("/")
 def index():
+    
     return render_template('chat.html')
 
 @app.route("/get", methods=["GET", "POST"])
